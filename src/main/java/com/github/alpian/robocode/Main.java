89b5f80d58bd052eb78c1de2e69405747f22ae65
Ian Davies <idda@amazon.com>
2017-10-26 19:09:34 +0200

    Who cares

---
+++ src/main/java/com/github/alpian/robocode/Main.java
@@ -0,0 +1,9 @@
+package com.github.alpian.robocode;
+
+import robocode.Robocode;
+
+public class Main {
+    public static void main(String[] args) {
+        Robocode.main(new String[] {});
+    }
+}
